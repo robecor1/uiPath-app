e1754c945c5f993dac7bdc7ae13da602bc475d73
Corolea Robert-Cozmin <robert.corolea@gmail.com>
2023-05-29 20:06:40 +0300

    feat: Added support for observable

--- src/app/components/t-grid/t-grid.component.ts
+++ src/app/components/t-grid/t-grid.component.ts
@@ -1,6 +1,6 @@
 import {Component, ContentChildren, Input, QueryList, Output, EventEmitter} from '@angular/core'
 import {CommonModule} from '@angular/common'
-import {Observable} from "rxjs";
+import {Observable, isObservable} from "rxjs";
 import {TColumnComponent} from "./components/t-column/t-column.component";
 import {TPaginationComponent} from "./components/t-pagination/t-pagination.component";
 import {DataPerKey, Direction, TGridDataItem} from "./@types";
@@ -59,22 +59,20 @@ export class TGridComponent {
   addDataToColumns(): void {
     if (this.children && this.data) {
       this.initDataKey()
-      this.populateDataKey()
+      this.prepareToPopulateDataKey()
       this.injectDataInChildren()
     }
   }
 
-  populateDataKey(): void {
-    // We should iterate over the data input once and build the "dataPerKey" object
-    // We do this as to not iterate it with a map for each child and pass the data
-    for (const item of this.data as TGridDataItem[]) {
-      // Iterate over the keys. These should be matched with the "property" input from the t-column template
-      for (const key of Object.keys(this.dataPerKey)) {
-        Object.assign(this.dataPerKey, {
-          // Add the value of the field to the array for that key ar create a new one with one item
-          [key]: [...this.dataPerKey[key as keyof DataPerKey<TGridDataItem>], item[key] ?? null]
-        })
-      }
+  prepareToPopulateDataKey(): void {
+    //Check if the data received is an observable and subscribe to it, processing the data as it comes
+    if (isObservable(this.data)) {
+      this.data.subscribe((data: TGridDataItem[]) => {
+        this.populateDataKey(data)
+      })
+    } else {
+      // If it's not proceed as usual
+      this.populateDataKey(this.data)
     }
   }
 
@@ -90,6 +88,20 @@ export class TGridComponent {
 
       child.globalSortable = this.sortable
       child.sortedChange = this.sortedChangeHandle.bind(this)
+    }
+  }
+
+  populateDataKey(data: TGridDataItem[]) {
+    // We should iterate over the data input once and build the "dataPerKey" object
+    // We do this as to not iterate it with a map for each child and pass the data
+    for (const item of data as TGridDataItem[]) {
+      // Iterate over the keys. These should be matched with the "property" input from the t-column template
+      for (const key of Object.keys(this.dataPerKey)) {
+        Object.assign(this.dataPerKey, {
+          // Add the value of the field to the array for that key ar create a new one with one item
+          [key]: [...this.dataPerKey[key as keyof DataPerKey<TGridDataItem>], item[key] ?? null]
+        })
+      }
     }
   }
 
