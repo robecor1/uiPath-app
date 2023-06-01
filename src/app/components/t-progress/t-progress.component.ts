518867e6723e2b7bf16f8138054b91714da5a1ff
Corolea Robert-Cozmin <robert.corolea@gmail.com>
2023-06-01 16:44:14 +0300

    feat: Adedd basic t-progress template

---
+++ src/app/components/t-progress/t-progress.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 't-progress',
+  standalone: true,
+  imports: [],
+  templateUrl: './t-progress.component.html',
+})
+export class TProgressComponent {
+}
--- src/app/pages/progress-circle/progress-circle.component.ts
+++ src/app/pages/progress-circle/progress-circle.component.ts
@@ -1,9 +1,11 @@
 import {Component} from '@angular/core';
+import {TProgressComponent} from "../../components/t-progress/t-progress.component";
 
 @Component({
   selector: 'app-progress-circle',
   standalone: true,
   imports: [
+    TProgressComponent
   ],
   templateUrl: './progress-circle.component.html',
 })
