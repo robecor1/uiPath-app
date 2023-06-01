7bea0b81c12b74a2dcd31b0ec833dee9d7141e3d
Corolea Robert-Cozmin <robert.corolea@gmail.com>
2023-06-01 16:00:51 +0300

    feat: Added route for the progress component

---
+++ src/app/pages/progress-circle/progress-circle.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-progress-circle',
+  standalone: true,
+  imports: [
+  ],
+  templateUrl: './progress-circle.component.html',
+})
+export class ProgressCircleComponent {
+}
