82c6ee3c1b5a467681eccdd44a8ad422dcef9c34
Corolea Robert-Cozmin <robert.corolea@gmail.com>
2023-06-02 20:14:43 +0300

    feat: fixed test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,6 +20,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('robert-app app is running!');
+    expect(compiled.querySelector('.content h1')?.textContent).toContain('UiPath assignment!');
   });
 });
